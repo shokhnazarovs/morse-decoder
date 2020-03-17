e1efe55ccd743c05e4f8a3e2954c0db7163fd40b
Shokhrukh Shokhnazarov <s.shokhnazarov@gmail.com>
2020-03-17 22:06:53 +0500

    feat: solve the problem

--- src/index.js
+++ src/index.js
@@ -38,7 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let answer = [];
+    let temp = '';
+    expr = expr.match(/.{1,10}/g);
+
+    for(let i=0; i<expr.length; i++){
+        temp = expr[i];
+        temp = temp.match(/.{1,2}/g);
+        for(let j=0; j<temp.length; j++){
+            if(temp[j] === '**'){
+                answer.push(" ");
+                break;
+            }
+            if(temp[j] === '00'){
+                temp.shift();
+                j--;
+                continue;
+            }
+            if(temp[j] === '10'){
+                temp[j] = '.';
+                continue;
+            }
+            if(temp[j] === '11'){
+                temp[j] = '-';
+                continue;
+            }
+        }
+
+        if(temp[0] != '**'){
+            temp = temp.join('');
+            answer.push(MORSE_TABLE[temp]);
+        }
+    }
+
+    return answer.join('');
 }
 
 module.exports = {
